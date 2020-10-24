beb2bf70a2c8b2f4e83fa65ed5d85c6b28aa4085
Ian J Sikes <ianjsikes@gmail.com>
2020-10-23 18:16:32 -0700

    Initial commit
    So let's see what we've got here. We have a basic React app, created using create-react-app.
    
    (Show dependencies)
    I've added
    - react-three-fiber and three
    - @react-three/drei, a library full of neat utilities for r3f
    - react-spring, an animation library that works well with r3f
    
    (Show styles.css)
    Here we have some very minimal css to make the React app fill the whole screen. We could change the background color here if we want.
    
    (Show index.js)
    Right now we are just rendering a header.

---
+++ src/ShapeCluster.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useFrame } from 'react-three-fiber'
+import { Shape } from './Shape'
+import { randomRange, Ease } from './utils'
+
+export const ShapeCluster = () => {
+  return null
+}
