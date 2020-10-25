157ffefbbf82d0e5ac02570f6f53cb7a6abedd11
Ian J Sikes <ianjsikes@gmail.com>
2020-10-25 16:53:39 -0700

    Varying animation speed
    Now our shapes are going up and down, but it'd be nice if they weren't
    in perfect unison. Another good opportunity for a random factor. Let's
    make a random offset for each shape. If we multiply it and time, we'll
    see our shapes moving at different rates. NICE.

--- src/Shape.js
+++ src/Shape.js
@@ -5,10 +5,11 @@ import { Ease, useRandomRange, Colors } from './utils'
 
 export const Shape = ({ position = [0, 0, 0] }) => {
   const meshRef = React.useRef()
+  const factor = useRandomRange(0.5, 1.5)
 
   useFrame((state) => {
     const time = state.clock.getElapsedTime()
-    const t = Math.sin(time)
+    const t = Math.sin(time * factor)
     meshRef.current.position.y = t
   })
 
