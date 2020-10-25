4ff71bffe3908b0fad86a5a792ef8dea909ca17b
Ian J Sikes <ianjsikes@gmail.com>
2020-10-25 16:57:09 -0700

    Scale anim and metalness
    In addition to up and down motion, we can also update the y-scale to
    give it a stretchy effect.
    
    While we are here, let's give our shapes a little metallic shine. With
    the standard material we can specify some physical properties. You can
    refer to the Three.js docs to see what they all are.

--- src/Shape.js
+++ src/Shape.js
@@ -13,12 +13,17 @@ export const Shape = ({ position = [0, 0, 0] }) => {
     t = Ease.inOutCubic((1 + t) / 2)
 
     meshRef.current.position.y = position[1] + t * 4
+    meshRef.current.scale.y = 1 + t * 3
   })
 
   return (
     <mesh ref={meshRef} position={position} castShadow receiveShadow>
       <sphereBufferGeometry args={[0.5, 16, 16]} />
-      <meshStandardMaterial color={Colors.shape} />
+      <meshStandardMaterial
+        color={Colors.shape}
+        roughness={0}
+        metalness={0.2}
+      />
     </mesh>
   )
 }
