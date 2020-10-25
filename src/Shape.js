dec1d07879660081ba5535a8a38c39969a6b7370
Ian J Sikes <ianjsikes@gmail.com>
2020-10-25 16:41:17 -0700

    Basic Animation
    Animation in react-three-fiber is interesting. I told you we'd be
    writing declarative code, and that is mostly true. However, for stuff
    like animation where we want to update every frame (16ms or so), we
    actually want to circumvent React and handle it imperatively. Our scene
    would slow to a crawl if we were rerendering a lot of React components
    that often.
    
    In the shape, we need a ref to the mesh. Then, we'll use a
    react-three-fiber hook called useFrame. We can pass it a function that
    will run every frame. The function will get passed a state object that
    has a lot of stuff on it, but most importantly it has the time elapsed
    since we started the scene.
    
    Animation is just changing a property over time. If I just say
    "meshref.current.position.y = time", we will see all of our shapes
    slowly rise up. Nice, but we can do better.

--- src/Shape.js
+++ src/Shape.js
@@ -4,8 +4,15 @@ import { useFrame } from 'react-three-fiber'
 import { Ease, useRandomRange, Colors } from './utils'
 
 export const Shape = ({ position = [0, 0, 0] }) => {
+  const meshRef = React.useRef()
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    meshRef.current.position.y = time
+  })
+
   return (
-    <mesh position={position} castShadow receiveShadow>
+    <mesh ref={meshRef} position={position} castShadow receiveShadow>
       <sphereBufferGeometry args={[0.5, 16, 16]} />
       <meshStandardMaterial color={Colors.shape} />
     </mesh>
