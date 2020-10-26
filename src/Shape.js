dfdaea4fd603cebccb28459e270699d9241f9228
Ian J Sikes <ianjsikes@gmail.com>
2020-10-25 17:15:22 -0700

    On click Shape
    Next, lets make each shape respond to click events. I'm thinking I want
    the shapes to change colors and size when clicked.
    
    Inside the shape component, we'll use the useState hook to keep track of
    whether or not our shape has been clicked. Let's add an onClick prop to
    the mesh to toggle active

--- src/Shape.js
+++ src/Shape.js
@@ -4,6 +4,7 @@ import { useFrame } from 'react-three-fiber'
 import { Ease, useRandomRange, Colors } from './utils'
 
 export const Shape = ({ position = [0, 0, 0] }) => {
+  const [active, setActive] = React.useState(false)
   const meshRef = React.useRef()
   const factor = useRandomRange(0.5, 1.5)
 
@@ -17,7 +18,13 @@ export const Shape = ({ position = [0, 0, 0] }) => {
   })
 
   return (
-    <mesh ref={meshRef} position={position} castShadow receiveShadow>
+    <mesh
+      ref={meshRef}
+      position={position}
+      onClick={() => setActive(!active)}
+      castShadow
+      receiveShadow
+    >
       <sphereBufferGeometry args={[0.5, 16, 16]} />
       <meshStandardMaterial
         color={Colors.shape}
